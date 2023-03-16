fefe45c89259e9118c503a9ad97ecb05ad6a4405
uytran36 <uytkg@fpt.com.vn>
2023-03-16 11:23:51 +0700

    feat(interceptors): ✨ create some interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthGuard } from './auth/auth.guard';
+import { ErrorsInterceptor } from './interceptors/error.interceptor';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalGuards(new AuthGuard());
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useGlobalInterceptors(new ErrorsInterceptor());
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,6 +10,7 @@ import {
   HttpStatus,
   UseGuards,
   SetMetadata,
+  UseInterceptors,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto, createUserSchema } from './dto/create-user.dto';
@@ -21,19 +22,21 @@ import { ParseIntPipe } from '@nestjs/common/pipes';
 import { JoiValidationPipe } from 'src/common/pipe/joi-validation.pipe';
 import { RolesGuard } from 'src/role/role.guard';
 import { Roles } from 'src/role/role.decorator';
+import { TransformInterceptor } from 'src/interceptors/transform.interceptor';
 
 @Controller('users')
 @UseGuards(RolesGuard)
 // @UseFilters(new HttpExceptionFilter()) // you can put this here
+@UseInterceptors(TransformInterceptor)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
   @UseFilters(HttpExceptionFilter)
   @UsePipes(new JoiValidationPipe(createUserSchema))
-  @SetMetadata('role', ['admin'])
   // with class-tranformer
   // create(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
+  @SetMetadata('role', ['admin'])
   create(@Body() createUserDto: CreateUserDto) {
     // throw new HttpException('error', HttpStatus.NOT_FOUND);
     return this.usersService.create(createUserDto);
